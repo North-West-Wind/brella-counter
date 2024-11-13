28f19c9e3b5a5b82106959f8ef97bdd713c70f03
NorthWestWind <wsyn148@gmail.com>
2024-11-13 10:30:51 +0800

    animated integrelle changing

--- src/components/BrellaColumn.tsx
+++ src/components/BrellaColumn.tsx
@@ -4,16 +4,23 @@ import "./BrellaColumn.css";
 import { multiRandomBoolean, randomBetween } from "../helper/math";
 import { useColor } from "../hooks/useColors";
 import { useHorizontal } from "../hooks/useHorizontal";
+import { wait } from "../helper/control";
+
+const TRANSITION_DELAY = 250;
 
 function BrellaColumn(props: { brellas: string[], integrelle: string }) {
 	const horizontal = useHorizontal();
 	const color = useColor();
 	const [svg, setSvg] = useState(`/integrelle/${props.integrelle}.svg`);
+	const [changing, setChanging] = useState(false);
 
 	useEffect(() => {
 		fetch(`/integrelle/${props.integrelle}.svg`).then(async res => {
 			if (res.ok) {
-				await new Promise(res => setTimeout(res, randomBetween(500, 1500, true)));
+				let delay = randomBetween(500, 1500, true) - TRANSITION_DELAY;
+				await wait(delay);
+				setChanging(true);
+				await wait(TRANSITION_DELAY);
 				const [eyeOpen, mouthOpen] = multiRandomBoolean(2);
 				const div = document.createElement("div");
 				div.innerHTML = await res.text();
@@ -22,12 +29,13 @@ function BrellaColumn(props: { brellas: string[], integrelle: string }) {
 				(div.querySelector("#mouth-open") as HTMLElement).style.display = mouthOpen ? "inline" : "none";
 				(div.querySelector("#mouth-close") as HTMLElement).style.display = !mouthOpen ? "inline" : "none";
 				setSvg('data:image/svg+xml;base64,' + btoa(div.innerHTML));
+				setChanging(false);
 			}
 		});
 	}, []);
 
 	const integrelleWrapper = <div className={"integrelle " + (horizontal ? "hori" : "vert") + (svg ? "" : " hidden")}>
-		<img src={svg} />
+		<img src={svg} className={(changing ? "changing" : "")} />
 	</div>
 
 	return <div className={horizontal ? "flex-dist" : ""}>
